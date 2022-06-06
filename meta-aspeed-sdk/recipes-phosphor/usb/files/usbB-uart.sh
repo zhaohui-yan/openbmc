9dd328661e22207bfa734bd63d0b3274cccc1023
Neal Liu <neal_liu@aspeedtech.com>
2022-06-06 13:49:09 +0800

    usb: refine usb gadgets
    Add usb gadgets for port A & port B:
    1. usb mass-storage
    2. usb cdc-acm
    3. usb cdc-ncm
    4. usb rndis
    
    Signed-off-by: Neal Liu <neal_liu@aspeedtech.com>
    Change-Id: If255852c5e461b55ac13f4e169b5e74062944740

---
+++ meta-aspeed-sdk/recipes-phosphor/usb/files/usbB-uart.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+GADGET_BASE=/sys/kernel/config/usb_gadget
+USB_UDC=/sys/bus/platform/devices/1e6a2000.usb
+NAME=uartusb
+
+free_port() {
+    local p
+    p=$(basename $USB_UDC)
+    if ! test -e $USB_UDC/gadget/suspended; then
+        echo $p
+        return
+    fi
+    echo "All ports taken" >&2
+    exit 1
+}
+
+
+usb_uart_create()
+{
+    if [ -d $GADGET_BASE/$NAME ]; then
+        echo "device $NAME already exists" >&2
+        return 1
+    fi
+    mkdir $GADGET_BASE/$NAME
+    cd $GADGET_BASE/$NAME
+
+    echo 0x1d6b > idVendor  # Linux Foundation
+    echo 0x0105 > idProduct # FunctionFS Gadget
+    mkdir strings/0x409
+    local serial="OBMCUART001"
+    echo $serial > strings/0x409/serialnumber
+    echo OpenBMC > strings/0x409/manufacturer
+    echo "OpenBMC Uart" > strings/0x409/product
+
+    mkdir configs/c.1
+    mkdir functions/acm.$NAME
+    mkdir configs/c.1/strings/0x409
+
+    echo "Config-1" > configs/c.1/strings/0x409/configuration
+    echo 120 > configs/c.1/MaxPower
+    ln -s functions/acm.$NAME configs/c.1
+    echo $(free_port) > UDC
+}
+
+if test "$1" = stop; then
+    rm -f $GADGET_BASE/$NAME/configs/c.1/acm.$NAME
+    rmdir $GADGET_BASE/$NAME/configs/c.1/strings/0x409
+    rmdir $GADGET_BASE/$NAME/configs/c.1
+    rmdir $GADGET_BASE/$NAME/functions/acm.$NAME
+    rmdir $GADGET_BASE/$NAME/strings/0x409
+    rmdir $GADGET_BASE/$NAME
+else
+    usb_uart_create
+fi
