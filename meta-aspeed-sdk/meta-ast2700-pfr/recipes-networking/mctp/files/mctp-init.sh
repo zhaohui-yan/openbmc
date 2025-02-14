72cc469fd59ed401072742fb2533932ec66a1e6e
Vince Chang <vince_chang@aspeedtech.com>
2025-02-14 09:56:49 +0800

    meta-aspeed-sdk: ast2700-dcscm: update MCTP over I2C init script
    Updated the MCTP over I2C initialization script for AST2700 A1 DCSCM board.
    AST2700 A1 DCSCM board also uses i2c10 for the PFR mailbox.
    
    For PCH-side SPDM attestation emulation, the AST1060 is used to emulate
    the PCH side. The BMC uses I2C8 to communicate with PFR over I2C5.
    
    Signed-off-by: Vince Chang <vince_chang@aspeedtech.com>
    Change-Id: If953192a647bac6afb023be9cb9e4272220b429c

--- meta-aspeed-sdk/meta-ast2700-pfr/recipes-networking/mctp/files/mctp-init.sh
+++ meta-aspeed-sdk/meta-ast2700-pfr/recipes-networking/mctp/files/mctp-init.sh
@@ -5,30 +5,32 @@ if [ -f /tmp/.mctp_init_done ];then
 fi
 
 cpu_rev_id=$(devmem 0x12c02000 32)
-io_rev_id=$(devmem 0x14c02000 32)
 
 # For BMC side spdm attestation emulation
-if [[ ${cpu_rev_id} == 0x06000003 && ${io_rev_id} == 0x06000003 ]]; then
-    # AST2700-A0
-    # BMC i2c10 -> AST1060 i2c5
-    mctp link set mctpi2c10 up mtu 68
-    mctp addr add 0x0a dev mctpi2c10
-    mctp route add 0x0b via mctpi2c10
-    mctp neigh add 0x0b dev mctpi2c10 lladdr 0x38
-else
-    # AST2700-Ax
-    # BMC i2c11 -> AST1060 i2c0
-    mctp link set mctpi2c11 up mtu 68
-    mctp addr add 0x0a dev mctpi2c11
-    mctp route add 0x0b via mctpi2c11
-    mctp neigh add 0x0b dev mctpi2c11 lladdr 0x38
-fi
+# AST2700-A0 DCSCM BMC i2c10 -> AST1060 i2c5
+# AST2700-A1 DCSCM BMC i2c10 -> AST1060 i2c0
+mctp link set mctpi2c10 up mtu 68
+mctp addr add 0x0a dev mctpi2c10
+mctp route add 0x0b via mctpi2c10
+mctp neigh add 0x0b dev mctpi2c10 lladdr 0x38
 
 # For PCH side spdm attestation emulation
-# BMC i2c15 -> AST1060 i2c2
-mctp link set mctpi2c15 net 2 up mtu 68
-mctp addr add 0x0a dev mctpi2c15
-mctp route add 0x0b via mctpi2c15
-mctp neigh add 0x0b dev mctpi2c15 lladdr 0x70
+# Using AST1060 to emulate PCH side spdm attestation
+if [[ $(( (cpu_rev_id >> 24) & 0xFF )) -eq 0x06 && \
+      $(( (cpu_rev_id >> 16) & 0xFF )) -eq 0x00 ]]; then
+    # AST2700-A0 DCSCM
+    # BMC i2c15 -> AST1060 i2c2
+    mctp link set mctpi2c15 net 2 up mtu 68
+    mctp addr add 0x0a dev mctpi2c15
+    mctp route add 0x0b via mctpi2c15
+    mctp neigh add 0x0b dev mctpi2c15 lladdr 0x70
+else
+    # AST2700-A1 DCSCM
+    # BMC i2c8 -> AST1060 i2c5
+    mctp link set mctpi2c8 net 2 up mtu 68
+    mctp addr add 0x0a dev mctpi2c8
+    mctp route add 0x0b via mctpi2c8
+    mctp neigh add 0x0b dev mctpi2c8 lladdr 0x70
+fi
 
 touch /tmp/.mctp_init_done
